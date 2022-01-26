42dad29b94ea80f45706f4bd00ed184d8d2efbb5
KettleOfFish <kettleoffish42@gmail.com>
2022-01-26 20:20:55 +0000

    ready to gut

--- ISSComp.py
+++ ISSComp.py
@@ -1,116 +1,39 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import PIL
-import pathlib
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.python.keras.optimizer_v2 import adam
-from tensorflow.python.keras.backend import sparse_categorical_crossentropy
+r"""A demo using `ClassificationEngine` to classify an image.
+You can run this example with the following command, which uses a
+MobileNet model trained with the iNaturalist birds dataset, so it's
+great at identifying different types of birds.
+python3 classify_image.py \
+--model models/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite \
+--label models/inat_bird_labels.txt \
+--image images/parrot.jpg
+"""
 
-# Defining Dataset
+import argparse
+from edgetpu.classification.engine import ClassificationEngine
+from edgetpu.utils import dataset_utils
+from PIL import Image
 
-dataset = open("ImageData").read()
-print(dataset)
-dataDir = pathlib.Path(dataset)
 
-image_count = len(list(dataDir.glob('*/*.png')))
-print(image_count)
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--model', help='File path of Tflite model.', required=True)
+  parser.add_argument('--label', help='File path of label file.', required=True)
+  parser.add_argument(
+      '--image', help='File path of the image to be recognized.', required=True)
+  args = parser.parse_args()
 
-# Data refining
+  # Prepare labels.
+  labels = dataset_utils.read_label_file(args.label)
+  # Initialize engine.
+  engine = ClassificationEngine(args.model)
+  # Run inference.
+  img = Image.open(args.image)
+  for result in engine.classify_with_image(img, top_k=3):
+    print('---------------------------')
+    print(labels[result[0]])
+    print('Score : ', result[1])
 
-batch_size = 32
-img_height = 180
-img_width = 180
 
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  dataDir,
-  validation_split=0.1,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  dataDir,
-  validation_split=0.1,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
-class_names = train_ds.class_names
-#print(class_names)
-
-# Data augmenting
-
-dataAugmentation = tf.keras.Sequential(
-  [
-    layers.experimental.preprocessing.RandomFlip("horizontal", 
-                                                 input_shape=(img_height, 
-                                                              img_width,
-                                                              3)),
-    layers.experimental.preprocessing.RandomRotation(0.1),
-    layers.experimental.preprocessing.RandomZoom(0.1),
-  ]
-)
-
-# Visualising
-
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-
-# Optimisation
-
-AUTOTUNE = tf.data.AUTOTUNE
-
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
-
-# Model Architecture
-
-numClasses = 42
-
-model = Sequential([
-  dataAugmentation,
-  layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Dropout(0.2),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(numClasses)
-])
-model.compile(loss=sparse_categorical_crossentropy(from_logits=True), optimizer=adam, metrics=["acc"])
-history = model.fit(train_ds, val_ds, epochs=2, verbose=1)
-
-print(model)
-
-# Class prediction
-
-issImagesURL = ""
-issImagesPath = tf.keras.utils.get_file('', origin=issImagesURL)
-
-img = tf.keras.preprocessing.image.load_img(
-    issImagesPath, target_size=(img_height, img_width)
-)
-img_array = tf.keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-
-print(
-    "This image most likely contains {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+if __name__ == '__main__':
+  main()
