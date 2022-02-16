7ba1b474b7d6f757866417490f222e9a3926d6f6
Kilian <30512397+kilianplapp@users.noreply.github.com>
2022-02-16 13:07:23 +0000

    added image data to data.txt

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import time
 from sense_hat import SenseHat
 import config
+import csv
 from picamera import PiCamera
 from orbit import ISS
-
+print('starting setup!')
 startTime = int(time.time())
 
-#Setup Camera and SenseHAT
-camera = PiCamera()
-camera.resolution = (1296,972)
-camera.start_preview()
-
+print('setting up sensehat')
 #SenseHAT not used, but makes it easy to tell if code runs.
 sense = SenseHat()
 sense.set_pixels(config.image)
 
+print('defining functions')
 def convert(angle):
     """
     Convert a `skyfield` Angle to an EXIF-appropriate
@@ -28,28 +26,36 @@ def convert(angle):
     exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
     return sign < 0, exif_angle
 
-def takePhoto(iteration):
+def takePhoto(iteration, camera):
     """Take a photo from the PiCamera"""
+    print('taking photo')
     point = ISS.coordinates()
-
     # Convert the latitude and longitude to EXIF-appropriate representations
     south, exif_latitude = convert(point.latitude)
     west, exif_longitude = convert(point.longitude)
-
+    time.sleep(2)
     # Set the EXIF tags specifying the current location
+    with open('data.txt', 'a') as file:
+        stringToWrite = f"\n image-{iteration}.jpg: {point.latitude} {point.longitude}" 
+        file.write(stringToWrite)
+        file.close()
     camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
     camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
     camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
     camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-    camera.capture(f"{config.base_folder}/image-{iteration}.jpg")
+    camera.capture(f"{config.base_folder}/data/image-{iteration}.jpg")
 
 def main():
     """Main loop to run any functions."""
+    cam = PiCamera()
+    cam.resolution = (1296,972)
+    #cam.start_preview()
     iteration = 0
     while int(time.time()) - startTime < config.runTime:
         iteration += 1
-        takePhoto(iteration)
+        takePhoto(iteration, cam)
         time.sleep(1)
 
+print('starting main function loop')
 if __name__ == "__main__":
     main()
